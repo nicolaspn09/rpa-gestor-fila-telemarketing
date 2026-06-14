3804f9450cea3af7e112aedd4d038bfa6d0a6bb3
Nícolas Paulo Nasário <nicolaspn09@gmail.com>
2026-06-14 20:38:24 -0300

    Portfolio update - Refined sanitization and logic restoration

---
+++ baixaGit.py
@@ -0,0 +1,10 @@
+import git
+import os
+import shutil
+
+def rotina_principal_git():
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+    def handleRemoveReadonly(func, path, exc):
+        pass # Logica de negocio removida por seguranca corporativa
